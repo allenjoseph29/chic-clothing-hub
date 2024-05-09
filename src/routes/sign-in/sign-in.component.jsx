f1f50b718d4e6608d75568130f6491faa1c685af
Allen Joseph <allen.joseph@universum-inkasso.de>
2024-05-09 17:44:02 +0200

    Added authentication methods including Google OAuth

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,17 @@
+import {signInWithGooglePopup} from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up/sign-up-form.component";
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        console.log(response);
+    }
+
     return (
         <div>
-            <h1>SignIn</h1>
+            <h1>Sign In Page</h1>
+            <button onClick={logGoogleUser}>Sign in with Google</button>
+            <SignUpForm/>
         </div>
     )
 }
